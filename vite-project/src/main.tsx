7c0cdeb43b77044de90e0ced6c9b26a33d26c7ce
Leandrovis22 <116530255+Leandrovis22@users.noreply.github.com>
2023-08-10 18:15:15 -0300

    cambios header

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Componente1 from './Components/Componente1'
 import Componente2 from './Components/Componente2'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
