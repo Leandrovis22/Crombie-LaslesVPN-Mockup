2eb5ac9457331fad1eb1a52171af47cbd4fe78af
Leandrovis22 <116530255+Leandrovis22@users.noreply.github.com>
2023-08-16 15:40:13 -0300

    header

--- vite-project/src/Components/Componente1.tsx
+++ vite-project/src/Components/Componente1.tsx
@@ -16,15 +16,14 @@ const Componente1: React.FC = () => {
     <div>
 
       <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <nav className="mt-5 ml-12 mr-12 px-4 lg:px-6 py-2.5">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="#" className="flex items-center">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+              <img src="src\Screenshot_1.png" className="h-12"/>
             </a>
             <div className="flex items-center lg:order-2">
-              <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
-              <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get started</a>
+              <a href="#" className="text-black-800 hover:bg-red-50 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Sign in</a>
+              <a href="#" className="border border-red-500 hover:bg-gray-50 text-red-500 font-medium py-2 px-8 rounded-full bg-transparent">Sign up</a>
               <button ref={buttonRef} data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false" onClick={handleClick}>
                 <span className="sr-only">Open main menu</span>
                 <svg className="w-6 h-6" fill="currentColor"
@@ -38,24 +37,24 @@ const Componente1: React.FC = () => {
               </button>
             </div>
             <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:mt-0">
                 <li>
-                  <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                  <a href="#" className="block py-2 pr-4 pl-3 text-black-700  border-gray-100 hover:bg-red-50">Home</a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Company</a>
+                  <a href="#" className="block py-2 pr-4 pl-3 text-black-700 border-gray-100 hover:bg-red-50">Company</a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Marketplace</a>
+                  <a href="#" className="block py-2 pr-4 pl-3 text-black-700  border-gray-100 hover:bg-red-50">Marketplace</a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
+                  <a href="#" className="block py-2 pr-4 pl-3 text-black-700 border-gray-100 hover:bg-red-50">Features</a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Team</a>
+                  <a href="#" className="block py-2 pr-4 pl-3 text-black-700  border-gray-100 hover:bg-red-50">Team</a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                  <a href="#" className="block py-2 pr-4 pl-3 text-black-700  border-gray-100 hover:bg-red-50">Contact</a>
                 </li>
               </ul>
             </div>
